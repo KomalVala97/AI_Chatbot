c3ca96dc012b4ce39af365f9b27511f6354bf4bd
Komal <your name>
2026-05-31 15:38:05 +0530

    feat:add conversation memory to chatbot

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ client = Groq(
     api_key=os.getenv("GROQ_API_KEY")
 )
 
+messages = [
+    {
+        "role": "system",
+        "content": "You are a helpful assistant."
+    }
+]
+
 while True:
 
     user_input = input("You: ")
@@ -16,16 +23,25 @@ while True:
         print("Chat ended.")
         break
 
+    messages.append(
+        {
+            "role": "user",
+            "content": user_input
+        }
+    )
+
     response = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": user_input
-            }
-        ],
+        messages=messages,
         model="llama-3.1-8b-instant"
     )
 
     ai_reply = response.choices[0].message.content
+
+    messages.append(
+        {
+            "role": "assistant",
+            "content": ai_reply
+        }
+    )
 
     print("AI:", ai_reply)
