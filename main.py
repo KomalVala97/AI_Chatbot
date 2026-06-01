6149065f18d228201386c47c1c2133820df43b53
Komal <your name>
2026-06-01 16:59:48 +0530

    feat: add web search and upgrade chatbot model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from groq import Groq
 from dotenv import load_dotenv
+from search import search_web
 import os
 
 load_dotenv()
@@ -11,7 +12,13 @@ client = Groq(
 messages = [
     {
         "role": "system",
-        "content": "You are a helpful assistant."
+        "content": """
+You are a helpful AI assistant.
+
+Use internet search results when available.
+Answer naturally.
+If search data is missing, say you are uncertain.
+"""
     }
 ]
 
@@ -23,25 +30,58 @@ while True:
         print("Chat ended.")
         break
 
-    messages.append(
-        {
-            "role": "user",
-            "content": user_input
-        }
-    )
+    try:
 
-    response = client.chat.completions.create(
-        messages=messages,
-        model="llama-3.1-8b-instant"
-    )
+        web_results = search_web(user_input)
 
-    ai_reply = response.choices[0].message.content
+        search_text = ""
 
-    messages.append(
-        {
-            "role": "assistant",
-            "content": ai_reply
-        }
-    )
+        for r in web_results:
 
-    print("AI:", ai_reply)
+            title = r.get("title", "")
+            body = r.get("body", "")
+
+            search_text += (
+                f"Title: {title}\n"
+                f"Content: {body}\n\n"
+            )
+
+        final_prompt = f"""
+User Question:
+{user_input}
+
+Internet Results:
+{search_text}
+"""
+
+        messages.append(
+            {
+                "role": "user",
+                "content": final_prompt
+            }
+        )
+
+        response = client.chat.completions.create(
+            messages=messages,
+            model="llama-3.3-70b-versatile"
+        )
+
+        ai_reply = (
+            response
+            .choices[0]
+            .message
+            .content
+        )
+
+        messages.append(
+            {
+                "role": "assistant",
+                "content": ai_reply
+            }
+        )
+
+        print("\nAI:", ai_reply)
+
+    except Exception as e:
+
+        print("\nError:", e)
---
+++ search.py
@@ -0,0 +1,14 @@
+from ddgs import DDGS
+
+def search_web(query):
+
+    with DDGS() as ddgs:
+
+        results = list(
+            ddgs.text(
+                query,
+                max_results=3
+            )
+        )
+
+    return results
